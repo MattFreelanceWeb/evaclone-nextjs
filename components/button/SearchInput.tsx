576605ad5dc8f7a8346c5dce1c8d6690459635d6
MattFreelanceWeb <matt.freelance.web@gmail.com>
2023-03-16 17:00:42 +0100

    composant Arena 1 stade de recherche fait; faire la partie du type de jeux choisit

--- components/button/SearchInput.tsx
+++ components/button/SearchInput.tsx
@@ -1,5 +1,5 @@
 import IonIcon from '@reacticons/ionicons'
-import React, { useState } from 'react'
+import React from 'react'
 
 type Props = {content:string , updateContent:Function}
 
@@ -9,6 +9,7 @@ function SearchInput({content, updateContent}: Props) {
     <div className='flex items-center gap-4 bg-[#0c0c10] border-2 p-2 '>
         <IonIcon name='search' size='large' />
         <input type="search" name="" id="" placeholder='rechercher' className="font-bold  border-white w-full  flex items-start bg-transparent uppercase" value={content} onChange={(e)=>{updateContent(e.target.value)}}/>
+        <span onClick={()=>{updateContent('')}} className={` cursor-pointer ${content === '' ? 'hidden' :'flex'}`}><IonIcon name='close-circle-outline' size='large'/></span>
     </div>
   )
 }
--- components/section/oujouer/ArenaSelect.tsx
+++ components/section/oujouer/ArenaSelect.tsx
@@ -1,3 +1,4 @@
+import BtnPurple from "@/components/button/BtnPurple";
 import SearchInput from "@/components/button/SearchInput";
 import SelectInput from "@/components/button/SelectInput";
 import IonIcon from "@reacticons/ionicons";
@@ -32,6 +33,8 @@ function ArenaSelect({}: Props) {
 
   const cityUs = [{ name: "flower mound", cp: "tx" }];
 
+  const [toggleIndex, setToggleIndex] = useState<number | undefined>();
+  const [cityChoosen, setCityChoosen] = useState<string>("");
   const [region, setRegion] = useState<string>("Europe");
   const [searchContent, setSearchContent] = useState<string>("");
   const [cityArray, setCityArray] = useState<any[]>([]);
@@ -40,57 +43,121 @@ function ArenaSelect({}: Props) {
     {
       region === "Europe" ? setCityArray(cityEurope) : setCityArray(cityUs);
     }
-    if(searchContent !== ""){
-      let newCityArray = cityArray.filter((city) => city.name.toLowerCase().startsWith(searchContent.toLocaleLowerCase()) )
-      setCityArray(newCityArray)
+    if (searchContent !== "") {
+      let newCityArray = cityArray.filter((city) =>
+        city.name.toLowerCase().startsWith(searchContent.toLocaleLowerCase())
+      );
+      setCityArray(newCityArray);
     }
-  }, [region,searchContent]);
+  }, [region, searchContent]);
 
   return (
-    <section className="flex flex-col  justify-start  absolute top-0 left-0 z-50 bg-black text-white  w-screen min-h-screen">
-      <div className="flex items-center justify-start gap-8 p-12 pb-2 w-full border-b-2 border-white">
-        <Link className=" " href={"/"}>
-          <IonIcon name="arrow-back-outline" size="large" />
-        </Link>
+    <section
+      className={`flex flex-col  justify-start  absolute top-0 left-0 z-50   w-screen min-h-screen ${
+        cityChoosen ? "bg-white text-black" : "bg-black text-white"
+      } `}
+    >
+      <div
+        className={`flex items-center justify-start gap-8 p-12 pb-2 w-full border-b-2 ${
+          cityChoosen ? "border-black" : "border-white"
+        }`}
+      >
+        {cityChoosen ? (
+          <span onClick={()=>{setCityChoosen('')}} className='cursor-pointer'>
+            <IonIcon name="arrow-back-outline" size="large" />
+          </span>
+        ) : (
+          <Link className=" " href={"/"}>
+            <IonIcon name="arrow-back-outline" size="large" />
+          </Link>
+        )}
         <div className=" flex-grow flex flex-col items-start justify-center">
-          <h1 className="text-4xl font-bold">Choisissez une arène</h1>
+          <h1 className="text-4xl font-bold">
+            {cityChoosen ? `${cityChoosen}` : "Choisissez une arène"}
+          </h1>
           <h2 className="font-semibold text-gray-400">{region}</h2>
         </div>
         <Link href={"/"} className=" flex-grow flex items-center justify-end ">
           <IonIcon name="close" size="large" />
         </Link>
       </div>
-      <div className="self-center flex flex-col gap-2 px-12 py-4 ">
-        <h3 className="font-bold uppercase text-center">
-          trouver une arène dans un autre pays
-        </h3>
-        <SelectInput
-          content={[{ name: "Europe" }, { name: "U.S.A" }]}
-          iconName={"flag-outline"}
-          placeHolder={"Choisir une Région"}
-          Region={setRegion}
-        />
-      </div>
-      <div className="self-center flex flex-col gap-2 px-12 py-4 w-full md:w-[500px]">
-        <SearchInput content={searchContent} updateContent={setSearchContent} />
-      </div>
 
-      <div className="flex flex-col p-12 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-4">
-        {cityArray.map((item, i) => (
-          <Link
-            href={""}
-            key={i}
-            className="border-b-2 border-gray-400 flex items-center justify-between py-4 md:flex-col md:items-start"
-          >
-            <h3 className="text-2xl capitalize">
-              {item.name} - {item.cp}
+      {cityChoosen ? (
+        <div>hello {cityChoosen}</div>
+      ) : (
+        <>
+          <div className="self-center flex flex-col gap-2 px-12 py-4 ">
+            <h3 className="font-bold uppercase text-center">
+              trouver une arène dans un autre pays
             </h3>
-            <button className="flex items-center justify-center gap-1 font-semibold text-gray-400">
-              Détails <IonIcon name="chevron-forward-outline" />
-            </button>
-          </Link>
-        ))}
-      </div>
+            <SelectInput
+              content={[{ name: "Europe" }, { name: "U.S.A" }]}
+              iconName={"flag-outline"}
+              placeHolder={"Choisir une Région"}
+              Region={setRegion}
+            />
+          </div>
+          <div className="self-center flex flex-col gap-2 px-12 py-4 w-full md:w-[500px]">
+            <SearchInput
+              content={searchContent}
+              updateContent={setSearchContent}
+            />
+          </div>
+
+          <ul
+            className={`flex flex-col p-12 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-4 `}
+          >
+            {cityArray.map((item, i) => (
+              <li
+                key={i}
+                className={` overflow-hidden duration-300 ${
+                  toggleIndex === i ? "h-full " : " h-20 md:h-24"
+                }`}
+              >
+                <div className="border-b-2 border-gray-400 flex items-center justify-between py-4 md:flex-col md:items-start cursor-pointer ">
+                  <h3
+                    onClick={() => {
+                      setCityChoosen(`${item.name} - ${item.cp}`);
+                    }}
+                    className="text-2xl capitalize h-full w-full"
+                  >
+                    {item.name} - {item.cp}
+                  </h3>
+                  <button
+                    onClick={() => {
+                      setToggleIndex(i);
+                    }}
+                    className="flex items-center justify-start gap-1 font-semibold text-gray-400 w-full"
+                  >
+                    Détails{" "}
+                    <span
+                      className={`duration-300 grid place-items-center ${
+                        toggleIndex === i ? "rotate-90" : "rotate-0"
+                      } z-10`}
+                    >
+                      <IonIcon name="chevron-forward-outline" />
+                    </span>
+                  </button>
+                </div>
+                <div className={`flex flex-col p-4 gap-4 duration-500 z-0 `}>
+                  <div className="flex items-center justify-between ">
+                    <div className="flex flex-col">
+                      <div>adresse</div>
+                      <Link href={`tel:${""}`} className="hover:underline">
+                        telephone
+                      </Link>
+                    </div>
+                    <div className="flex-col">Arène</div>
+                  </div>
+                  <div>
+                    <BtnPurple content={"selectionner"} href="" />
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </section>
   );
 }
